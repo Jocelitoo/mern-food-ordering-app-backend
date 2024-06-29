528992f9c883fff5fe0217d02d5d91053125cbd8
Jocelito Climeres <juninhoclimeres@outlook.com>
2024-06-29 11:01:47 -0300

    correção no .env

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -14,7 +14,7 @@ declare global {
 }
 
 export const jwtCheck = auth({
-  audience: process.env.AUTH0_AUDICENTE,
+  audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: 'RS256',
 });
